20457916e126530c2c4068c259bb7df18797ad47
Bryan Garcia <bryan@promaticsoft.com>
2024-09-25 23:50:00 -0700

    feat: Declarar nuevo método para obtener contacto por su id en ContactRepository

--- backend/libreta-backend/app/Repositories/ContactRepository.php
+++ backend/libreta-backend/app/Repositories/ContactRepository.php
@@ -6,6 +6,7 @@ interface ContactRepository
 {
     public function createContact(array $contact);
     public function getAllContacts();
+    public function getContactById(int $id);
     public function updateContact(int $id, array $contact);
     public function deleteContact(int $id);
 }
