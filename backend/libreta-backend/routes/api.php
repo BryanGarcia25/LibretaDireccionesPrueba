433f216be551584c32197f0389261fc88aa3c79a
Bryan Garcia <bryan@promaticsoft.com>
2024-09-25 23:23:40 -0700

    feat: Añadir ruta para actualizar contacto

--- backend/libreta-backend/routes/api.php
+++ backend/libreta-backend/routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/create', [ContactController::class, 'store']);
 Route::get('/contacts', [ContactController::class, 'index']);
+Route::put('/update/{id}', [ContactController::class, 'edit']);
