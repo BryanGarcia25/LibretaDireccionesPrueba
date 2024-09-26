4617e0451623c5e044db6017786d09fcdabc7845
Bryan Garcia <bryan@promaticsoft.com>
2024-09-25 19:15:23 -0700

    feat: Implementar método de la interfaz para crear contacto

--- backend/libreta-backend/app/Repositories/ContactRepositoryImpl.php
+++ backend/libreta-backend/app/Repositories/ContactRepositoryImpl.php
@@ -6,6 +6,11 @@ use App\Models\Contact;
 
 class ContactRepositoryImpl implements ContactRepository
 {
+    public function createContact(array $contact)
+    {
+        return Contact::create($contact);
+    }
+
     public function getAllContacts()
     {
         return Contact::with(['Phones', 'Emails', 'Addresses'])->get();
