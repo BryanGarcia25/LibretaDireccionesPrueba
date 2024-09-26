9ced8fab2346442074ebcccea6fff1eb37c6ac1a
Bryan Garcia <bryan@promaticsoft.com>
2024-09-25 23:34:26 -0700

    feat: Agregar ruta para eliminar contacto

--- backend/libreta-backend/routes/api.php
+++ backend/libreta-backend/routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/create', [ContactController::class, 'store']);
 Route::get('/contacts', [ContactController::class, 'index']);
 Route::put('/update/{id}', [ContactController::class, 'edit']);
+Route::delete('/delete/{id}', [ContactController::class, 'destroy']);
