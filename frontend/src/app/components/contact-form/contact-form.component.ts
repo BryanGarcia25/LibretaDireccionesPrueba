906f0ecebfc14805b84c00235a83f146b01cbec3
Bryan Garcia <bryan@promaticsoft.com>
2024-09-26 15:09:53 -0700

    feat: Añadir FormArray para guardar un arreglo de telefonos

--- frontend/src/app/components/contact-form/contact-form.component.ts
+++ frontend/src/app/components/contact-form/contact-form.component.ts
@@ -5,14 +5,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   providers: [provideNativeDateAdapter()],
   standalone: true,
-  imports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatDividerModule, ReactiveFormsModule, MatDatepickerModule],
+  imports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatDividerModule, ReactiveFormsModule, MatDatepickerModule, MatFormFieldModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
@@ -28,8 +29,27 @@ export class ContactFormComponent {
       birthday: ['', Validators.required],
       website: [''],
       company: ['', Validators.required],
+      phones: this.formBuilder.array([
+        this.formBuilder.group({
+          phone_number: ['', Validators.required],
+        })
+      ]),
     })
 }
+
+  get phoneArray() : FormArray {
+    return this.contactForm.get('phones') as FormArray;
+  }
+
+  addPhoneNumber() {
+    this.phoneArray.push(this.formBuilder.group({
+      phone_number: ['', Validators.required],
+    }));
+  }
+
+  removePhoneNumber(index: number) {
+    this.phoneArray.removeAt(index);
+  }
 
   onSubmitContactForm() {
     
