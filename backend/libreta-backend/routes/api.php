971769dd2a9ccacc451b08e3c14962f75a5555e7
Bryan Garcia <bryan@promaticsoft.com>
2024-09-25 23:51:32 -0700

    feat: Agregar ruta para obtener contacto por su id

--- backend/libreta-backend/routes/api.php
+++ backend/libreta-backend/routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/create', [ContactController::class, 'store']);
 Route::get('/contacts', [ContactController::class, 'index']);
+Route::get('/contact/{id}', [ContactController::class, 'show']);
 Route::put('/update/{id}', [ContactController::class, 'edit']);
 Route::delete('/delete/{id}', [ContactController::class, 'destroy']);
