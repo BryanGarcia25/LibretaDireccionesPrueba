ba72676dfeb33eb5f247c14e43eadd2173081007
Bryan Garcia <bryan@promaticsoft.com>
2024-09-26 10:02:11 -0700

    feat: Añadir método ngOnInit para obtener los datos del contacto en el servicio

--- frontend/src/app/pages/detail-contact/detail-contact.component.ts
+++ frontend/src/app/pages/detail-contact/detail-contact.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { ContactService } from '../../services/contact.service';
+import { ActivatedRoute } from '@angular/router';
+import { Contact } from '../../models/contact.model';
 
 @Component({
   selector: 'app-detail-contact',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatDividerModule],
   templateUrl: './detail-contact.component.html',
   styleUrl: './detail-contact.component.css'
 })
-export class DetailContactComponent {
+export class DetailContactComponent implements OnInit {
+  detailContact!: Contact;
+
+  constructor(private contactService: ContactService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.contactService.getContactById(this.route.snapshot.paramMap.get('id')!).subscribe({
+      next: (response) => {
+        this.detailContact = response;
+      },
+      error: (error) => {
+        alert(`Error al obtener contacto ${error}`)
+      }
+    })
+  }
 
 }
