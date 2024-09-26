80c953f93ba38b17bf4538745a5eedef977139db
Bryan Garcia <bryan@promaticsoft.com>
2024-09-25 23:31:50 -0700

    feat: Declarar nuevo método para eliminar un contacto en ContactRepository

--- backend/libreta-backend/app/Repositories/ContactRepository.php
+++ backend/libreta-backend/app/Repositories/ContactRepository.php
@@ -7,4 +7,5 @@ interface ContactRepository
     public function createContact(array $contact);
     public function getAllContacts();
     public function updateContact(int $id, array $contact);
+    public function deleteContact(int $id);
 }
