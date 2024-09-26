5664d89f57df1784459851d2f28224279961e0e1
Bryan Garcia <bryan@promaticsoft.com>
2024-09-26 02:44:12 -0700

    feat: Crear tabla para mostrar contactos

--- frontend/src/app/components/contact-list/contact-list.component.ts
+++ frontend/src/app/components/contact-list/contact-list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ContactService } from '../../services/contact.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Contact } from '../../models/contact.model';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginator, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
-export class ContactListComponent {
 
+export class ContactListComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'notes', 'birthday', 'website', 'company'];
+  contacts = new MatTableDataSource<Contact>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.contactService.getAllContacts().subscribe({
+      next: (response) => {
+        this.contacts.data = response;
+        this.contacts.paginator = this.paginator;
+      },
+      error: (error) => {
+        alert(`Error en el servidor ${error}`)
+      }
+    });
+  }
 }
