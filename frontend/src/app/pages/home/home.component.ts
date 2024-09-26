743fa254312fcce4e5ea15eab4781b8da09f2f76
Bryan Garcia <bryan@promaticsoft.com>
2024-09-26 02:54:07 -0700

    feat: Añadir botón para registrar contactos

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactListComponent } from '../../components/contact-list/contact-list.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ContactListComponent],
+  imports: [ContactListComponent, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
