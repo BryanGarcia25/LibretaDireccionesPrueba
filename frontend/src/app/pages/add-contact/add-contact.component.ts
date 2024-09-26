77cfa4fcdc8f02583c45e8eb03bf7662e9cd61e6
Bryan Garcia <bryan@promaticsoft.com>
2024-09-26 03:08:51 -0700

    feat: Agregar contact-form component en add-contact component

--- frontend/src/app/pages/add-contact/add-contact.component.ts
+++ frontend/src/app/pages/add-contact/add-contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 
 @Component({
   selector: 'app-add-contact',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './add-contact.component.html',
   styleUrl: './add-contact.component.css'
 })
