be8a5548f489a7cd9853acc7589f90bb5cc134f5
Bryan Garcia <bryan@promaticsoft.com>
2024-09-26 03:25:55 -0700

    feat: Añadir formulario para capturar datos de contacto

--- frontend/src/app/components/contact-form/contact-form.component.ts
+++ frontend/src/app/components/contact-form/contact-form.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
+  providers: [provideNativeDateAdapter()],
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatDividerModule, ReactiveFormsModule, MatDatepickerModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  contactForm!: FormGroup
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      notes: [''],
+      birthday: ['', Validators.required],
+      website: [''],
+      company: ['', Validators.required],
+    })
+}
+
+  onSubmitContactForm() {
+    
+  }
 }
