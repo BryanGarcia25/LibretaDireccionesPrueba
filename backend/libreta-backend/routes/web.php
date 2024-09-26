d49c7871321a36ddf4994069e6fe4ff62d36808d
Bryan Garcia <bryan@promaticsoft.com>
2024-09-25 19:34:36 -0700

    fix: Restaurar el archivo web.php a su estado normal

--- backend/libreta-backend/routes/web.php
+++ backend/libreta-backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/contacts', [ContactController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
